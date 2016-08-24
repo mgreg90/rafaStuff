7f5c7d301d909ad26de32103b76009185edb98ac
Michael Gregory <mgregory8219@gmail.com>
2016-08-24 12:41:36 -0400

    changed to two different versions, by account balance and by contract return

--- max_drawdown_by_contract_return.rb
+++ max_drawdown_by_contract_return.rb
@@ -1,16 +1,18 @@
 # How to use:
-# run cmd: "ruby max_drawdown_by_account_balance.rb name_of_csv_file.csv"
+# run cmd: "ruby max_drawdown_by_contract_return.rb name_of_csv_file.csv"
 
-account_balances = []
+return_per_contract = []
 File.read("./#{ARGV[0]}").lines.each do |row|
-  account_balances << row.to_i
+  return_per_contract << row.to_i
 end
 
+account_balance = 0
 running_max_account_balance = 0
 running_min_account_balance = 0
 running_max_drawdown = 0
 running_max_upside = 0
-account_balances.each_with_index do |account_balance, index|
+return_per_contract.each_with_index do |current_contract, index|
+  account_balance += current_contract
   running_max_account_balance = account_balance if account_balance > running_max_account_balance
   running_min_account_balance = account_balance if account_balance < running_min_account_balance
   if (account_balance - running_max_account_balance) < running_max_drawdown
@@ -21,7 +23,7 @@ account_balances.each_with_index do |account_balance, index|
   end
 end
 
-puts "account balance: #{account_balances.last}"
+puts "account balance: #{account_balance}"
 puts "running max account balance: #{running_max_account_balance}"
 puts "running max drawdown: #{running_max_drawdown}"
 puts "running min account balance: #{running_min_account_balance}"
