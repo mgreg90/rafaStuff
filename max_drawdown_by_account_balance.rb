b2a8b094faf8e21829a92054b9af6bade123d8b6
Michael Gregory <mgregory8219@gmail.com>
2016-08-26 15:32:24 -0400

    added running_max_drawdown %

--- max_drawdown_by_account_balance.rb
+++ max_drawdown_by_account_balance.rb
@@ -10,19 +10,24 @@ running_max_account_balance = 0
 running_min_account_balance = 0
 running_max_drawdown = 0
 running_max_upside = 0
+running_max_drawdown_perc = nil
 account_balances.each_with_index do |account_balance, index|
   running_max_account_balance = account_balance if account_balance > running_max_account_balance
   running_min_account_balance = account_balance if account_balance < running_min_account_balance
   if (account_balance - running_max_account_balance) < running_max_drawdown
     running_max_drawdown = (account_balance - running_max_account_balance)
+    running_max_drawdown_perc = (running_max_drawdown.to_f/(account_balance - running_max_drawdown)) * 100 if (account_balance - running_max_drawdown) != 0
   end
   if (account_balance - running_min_account_balance) > running_max_upside
     running_max_upside = (account_balance - running_min_account_balance)
   end
 end
 
-puts "account balance: #{account_balances.last}"
-puts "running max account balance: #{running_max_account_balance}"
-puts "running max drawdown: #{running_max_drawdown}"
-puts "running min account balance: #{running_min_account_balance}"
-puts "running max upside: #{running_max_upside}"
+
+
+puts "account balance: $#{account_balances.last}"
+puts "running max account balance: $#{running_max_account_balance}"
+puts "running max drawdown: $#{running_max_drawdown}"
+puts "running max drawdown: #{running_max_drawdown_perc}%"
+puts "running min account balance: $#{running_min_account_balance}"
+puts "running max upside: $#{running_max_upside}"
